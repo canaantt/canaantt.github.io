562f561397e5e29257093511660c1ec8fb72ec09
Jenny Zhang <canaantt@gmail.com>
2016-01-01 20:54:43 -0800

    add glassdoor api

--- FE_P5/js/app.js
+++ FE_P5/js/app.js
@@ -15,29 +15,36 @@ function initMap() {
   });
 }
 
-// Class to represent a row in the seat reservations grid
-function SeatReservation(name, initialMeal) {
-    var self = this;
-    self.name = name;
-    self.meal = ko.observable(initialMeal);
-}
-
-// Overall viewmodel for this screen, along with initial state
-function ReservationsViewModel() {
-    var self = this;
-
-    // Non-editable catalog data - would come from the server
-    self.availableMeals = [
-        { mealName: "Standard (sandwich)", price: 0 },
-        { mealName: "Premium (lobster)", price: 34.95 },
-        { mealName: "Ultimate (whole zebra)", price: 290 }
-    ];    
-
-    // Editable data
-    self.seats = ko.observableArray([
-        new SeatReservation("Steve", self.availableMeals[0]),
-        new SeatReservation("Bert", self.availableMeals[0])
-    ]);
-}
-
-ko.applyBindings(new ReservationsViewModel());
+var $gldHeaderElem = $('#gldimes-header');
+var $gldElem = $('#gldimes-records');
+$("form").submit(function(event) {
+        var $city = $("#city").val();
+        var $street = $("#street").val();
+        $searchTerm = $city;
+        var $address = $street.trim() + ", " + $city;
+        console.log("address is :" + $address);
+        var imageTag = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '">';
+        $body.append(imageTag);
+    });
+//$searchTerm = $("#city").val();
+var $searchTerm = "pharmaceutical";
+var $partnerId = "51544";
+var $api_key = "eZ8t71dWbXs";
+var $publicIP = "76.22.47.171";
+var url = "http://api.glassdoor.com/api/api.htm?t.p=" + $partnerId + 
+           "&t.k=" + $api_key + 
+           "&userip=" + $publicIP + 
+           "&useragent=" + "Chrome" + "&format=json&v=1&action=employers" +
+           "&q=" + $searchTerm + "&city=Seattle" + "&ps=8";
+console.log("url is:" + url);
+$.ajax({
+      url: url,
+      dataType: "jsonp",
+      success: function(response){
+           var articles = response.response.employers;
+           console.log("Glassdoor Search Engine: ", articles);
+           articles.forEach(function(art){
+             $gldElem.append('<li><a target="_blank" href="http://' + art.website + '">' + art.name + '</a></li>');
+           });
+      }
+    });
