41e46c42c3eebaa7fcc02df3052a35a238487a2d
Jenny Zhang <canaantt@gmail.com>
2016-04-06 18:29:13 -0700

    started working on P4 web optimization

--- FE_P4/views/js/main.js
+++ FE_P4/views/js/main.js
@@ -498,14 +498,14 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
-var constance = Math.sin((document.body.scrollTop / 1250);
+var constance = Math.sin((document.body.scrollTop / 1250));
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = constance  + (i % 5));
+    var phase = constance  + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
