3fbdcfff3605dbd57d0c26246d4a62f745465916
Jenny Zhang <canaantt@gmail.com>
2016-04-18 09:37:12 -0700

    change api to streeteasy

--- FE_P5/js/app.js
+++ FE_P5/js/app.js
@@ -8,50 +8,49 @@ function initMap() {
   //      'address': address
   //};
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:47.6097, lng:-122.3331},
+    center: {lat:40.7589768, lng:-74.109316},
     //center: address.geometry.location,
     scrollwheel: true,
     zoom: 8
   });
 
+  var $token = "4851e3bfa40ae730be9fcd08ce7010e11b4e79cb";
+
+  var url = "http://streeteasy.com/nyc/api/areas/for_location?" +
+            "lon=" + "-74.0076" + "&lat=" + "40.7186&" + 
+            "key=" + $token + "&format=json";
 
 
-var lat = 47.6097;
-var $yelpHeaderElem = $('#yelpimes-header');
-var $yelpElem = $('#yelpimes-records');
-$("form").submit(function(event) {
-        var $city = $("#city").val();
-        var $street = $("#street").val();
-        $searchTerm = $city;
-        var $address = $street.trim() + ", " + $city;
-        console.log("address is :" + $address);
-        var imageTag = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '">';
-        $body.append(imageTag);
-    });
+
+// var lat = 47.6097;
+// var $yelpHeaderElem = $('#yelpimes-header');
+// var $yelpElem = $('#yelpimes-records');
+// $("form").submit(function(event) {
+//         var $city = $("#city").val();
+//         var $street = $("#street").val();
+//         $searchTerm = $city;
+//         var $address = $street.trim() + ", " + $city;
+//         console.log("address is :" + $address);
+//         var imageTag = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '">';
+//         $body.append(imageTag);
+//     });
 //$searchTerm = $("#city").val();
-var $consumer_key = "50FjBMSKJUh2OaE_fiVt2w";
-var $consumer_secret = "stqahr_NGTX0_PJvLxRVhBo7rsw";
-var $token = "4P9jlOJ_7H4s8lLgmzWLTzadzzckHxO9";
-var $token_secret = "Y07An4DVAfc152afg664HCWpexU";
-var $searchTerm = "pharmaceutical";
-var $location = "Seattle";
+// var $consumer_key = "50FjBMSKJUh2OaE_fiVt2w";
+// var $consumer_secret = "stqahr_NGTX0_PJvLxRVhBo7rsw";
+// var $token = "4P9jlOJ_7H4s8lLgmzWLTzadzzckHxO9";
+// var $token_secret = "Y07An4DVAfc152afg664HCWpexU";
+// var $searchTerm = "pharmaceutical";
+// var $location = "Seattle";
 
-var url = "https://api.yelp.com/v2/search?term=" + "food" + "&location=" + $location +
-          "&oauth_consumer_key=" + $consumer_key +
-          "&oauth_token=" + $token +
-          "&oauth_signature_method=" + "HMAC-SHA1" +
-          "&oauth_signature=" + $token + 
-          "&oauth_timestamp=" + Date.now() +  
-          "&oauth_nonce=" +
-          "&location=" + $location +'"';
 
-console.log("url is:" + url);
-$.ajax({
+
+ console.log("url is:" + url);
+ $.ajax({
       url: url,
       dataType: "jsonp",
       success: function(response){
            var articles = response.response.employers;
-           console.log("Glassdoor Search Engine: ", articles);
+           console.log("StreetEasy Search Engine: ", articles);
            articles.forEach(function(art){
              $yelpElem.append('<li><a target="_blank" href="http://' + art.website + '">' + art.name + '</a></li>');
              lat = lat - 0.1; 
@@ -62,5 +61,6 @@ $.ajax({
                               });
            });
       }
-    });
-}
+     });
+ }
+
